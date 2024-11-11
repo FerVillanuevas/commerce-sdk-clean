64c98c876d7722fe2063e13c76f0ec4e81afa963
Fermin Villanueva <FerVillanuev4s@gmail.com>
2024-11-11 11:18:10 -0700

    Init

--- dist/config.mjs
+++
@@ -1,6 +0,0 @@
-// src/config.ts
-var CUSTOM_API_DEFAULT_BASE_URI = "https://{shortCode}.api.commercecloud.salesforce.com/custom/{apiName}/{apiVersion}";
-export {
-  CUSTOM_API_DEFAULT_BASE_URI
-};
-//# sourceMappingURL=config.mjs.map
